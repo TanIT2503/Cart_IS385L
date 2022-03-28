ea3a819497a52d2252278644caa27727da65e74c
maitthanhtruc <maitthanhtruc282001@gmail.com>
2022-03-29 02:08:43 +0700

    GioHang and ThongTinChiTiet

--- Cart/Cart/GioHang.aspx.cs
+++ Cart/Cart/GioHang.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -13,7 +14,32 @@ namespace Cart
         {
 
         }
+        protected void del_pro(object source, DataListCommandEventArgs e)//ham xoa dư lieu khoi bang
+        {
+            if (e.CommandName == "Delete_command")
+            {
 
+                string ID_delete = DataList2.DataKeys[e.Item.ItemIndex].ToString();
+                // Kết nối dữ liệu
+                String url = Server.MapPath("App_Data/CART_IS385L.mdf");
+                String strconn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + url + ";Integrated Security=True";
+                //C:\Users\LENOVO\Documents\WebApplicationProjects\Doannhom\Cart_IS385L\Cart\Cart\;Integrated Security=True
+
+                // Sử dụng đối tượng kết nối SQL
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = strconn;
+                con.Open();
+
+                string sql_command_delete =
+                    "DELETE FROM CART WHERE MADONHANG=N'" + ID_delete + "';";
+                SqlCommand lenhthem = new SqlCommand();
+                lenhthem.Connection = con;
+                lenhthem.CommandType = System.Data.CommandType.Text;
+                lenhthem.CommandText = sql_command_delete;
+                lenhthem.ExecuteNonQuery();
+                Response.Redirect("GioHang.aspx");
+            }
+        }
         protected void DataList3_SelectedIndexChanged(object sender, EventArgs e)
         {
 
--- Cart/Cart/GioHang.aspx.designer.cs
+++ Cart/Cart/GioHang.aspx.designer.cs
@@ -60,6 +60,33 @@ namespace Cart
         protected global::System.Web.UI.WebControls.DataList DataList2;
 
         /// <summary>
+        /// DataList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList DataList1;
+
+        /// <summary>
+        /// SUM control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SUM;
+
+        /// <summary>
+        /// lbThanhToan control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbThanhToan;
+
+        /// <summary>
         /// CART control.
         /// </summary>
         /// <remarks>
